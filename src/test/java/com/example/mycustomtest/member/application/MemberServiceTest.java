c96e7a79f68218d235105bbec9ad13c0ddbb4c93
jimin.joo <wnwlals22@naver.com>
2021-08-09 01:16:10 +0900

    test: add MemberService test

---
+++ src/test/java/com/example/mycustomtest/member/application/MemberServiceTest.java
@@ -0,0 +1,71 @@
+package com.example.mycustomtest.member.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import com.example.mycustomtest.member.domain.Member;
+import com.example.mycustomtest.member.dto.MemberRequest;
+import com.example.mycustomtest.member.dto.MemberResponse;
+import com.example.mycustomtest.member.infrastructure.MemberRepository;
+import java.util.List;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @InjectMocks
+    MemberService memberService;
+
+
+    @DisplayName("멤버 전체 조회")
+    @Test
+    void getMembersTest() {
+        // given
+        Mockito.when(memberRepository.findAll()).thenReturn(List.of(Member.builder().name("주지민").age(30).address("구로구").build(),
+                                                                    Member.builder().name("주지민").age(31).address("광진구").build()));
+
+        // when
+        List<MemberResponse> actual = memberService.getMembers();
+
+        // then
+        assertThat(actual).isNotEmpty();
+        assertThat(actual).extracting("name","age","address")
+                          .contains(tuple("주지민",30,"구로구"),
+                                    tuple("주지민",31,"광진구"));
+
+        Mockito.verify(memberRepository).findAll();
+    }
+
+    @DisplayName("멤버 저장")
+    @Test
+    void saveMemberTest() {
+        // given
+        MemberRequest memberRequest = new MemberRequest("주지민", 30, "구로구");
+        Mockito.when(memberRepository.save(Mockito.any())).thenReturn(Member.builder()
+                                                                            .name("주지민")
+                                                                            .age(30)
+                                                                            .address("구로구")
+                                                                            .build());
+
+        // when
+        MemberResponse actual = memberService.saveMember(memberRequest);
+
+        // then
+        assertThat(actual).isNotNull();
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertThat(actual.getName()).isEqualTo("주지민");
+            assertThat(actual.getAge()).isEqualTo(30);
+            assertThat(actual.getAddress()).isEqualTo("구로구");
+        });
+    }
+}
