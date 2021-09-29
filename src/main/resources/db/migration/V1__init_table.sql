8fbe6d6f37b00950d38b667fd63d4c78430cfb14
jimin.joo <wnwlals22@naver.com>
2021-09-29 21:46:16 +0900

    feat(Embedded MySQL): Wix MySQL configuration setting

--- src/main/resources/db/migration/V1__init_table.sql
+++ src/main/resources/db/migration/V1__init_table.sql
@@ -1,9 +1,9 @@
 create table member (
-    id bigint not null AUTO_INCREMENT,
-    address varchar(255) not null,
-    age integer not null,
-    name varchar(255) not null,
-    primary key (id),
-    create_time timestamp,
-    update_time timestamp
-);
+    `id` bigint not null AUTO_INCREMENT,
+    `address` varchar(255) not null,
+    `age` int not null,
+    `name` varchar(255) not null,
+    `create_time` datetime(6) NOT NULL,
+    `update_time` datetime(6) NOT NULL,
+    primary key (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
---
+++ src/test/java/com/example/mycustomtest/embeddeddb/MemberEmbeddedSupport.java
@@ -0,0 +1,34 @@
+package com.example.mycustomtest.embeddeddb;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.example.mycustomtest.member.domain.Member;
+import com.example.mycustomtest.member.infrastructure.MemberRepository;
+import java.util.List;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Disabled
+public class MemberEmbeddedSupport extends EmbeddedDbSupport {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void test() {
+        // given
+        memberRepository.save(new Member("주지민1", 31, "서울시"));
+        memberRepository.save(new Member("주지민2", 31, "서울시"));
+        memberRepository.save(new Member("주지민3", 31, "서울시"));
+
+        // when
+        List<Member> actual = memberRepository.findAll();
+
+        // then
+        assertThat(actual)
+            .hasSize(3)
+            .extracting("name")
+            .containsExactly("주지민1", "주지민2", "주지민3");
+    }
+}
